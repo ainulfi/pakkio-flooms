feaa66ca8d5eaa5c6e1e68a20b3d60642692ea6e
Mohammad Ainul <127191017+ainulfi@users.noreply.github.com>
2026-04-28 14:07:16 +0700

    fix ref

--- app/Views/template/view_header.php
+++ app/Views/template/view_header.php
@@ -7,27 +7,27 @@
 	<meta name="viewport" content="width=device-width, initial-scale=1">
 	<!-- <meta http-equiv="refresh" content="20"> -->
 	<!--favicon-->
-	<link rel="icon" href="<?= base_url('images/favicon-32x32.png') ?>" type="image/png" />
+	<link rel="icon" href="<?= base_url('/images/favicon-32x32.png') ?>" type="image/png" />
 	<!--plugins-->
-	<link href="<?= base_url('plugins/vectormap/jquery-jvectormap-2.0.2.css') ?>" rel="stylesheet"/>
-	<link href="<?= base_url('plugins/simplebar/css/simplebar.css') ?>" rel="stylesheet" />
-	<link href="<?= base_url('plugins/perfect-scrollbar/css/perfect-scrollbar.css') ?>" rel="stylesheet" />
-	<link href="<?= base_url('plugins/metismenu/css/metisMenu.min.css') ?>" rel="stylesheet" />
-	<link href="<?= base_url('plugins/datatable/css/dataTables.bootstrap5.min.css') ?>" rel="stylesheet" />
-	<link href="<?= base_url('plugins/highcharts/css/highcharts.css') ?>" rel="stylesheet" />
+	<link href="<?= base_url('/plugins/vectormap/jquery-jvectormap-2.0.2.css') ?>" rel="stylesheet"/>
+	<link href="<?= base_url('/plugins/simplebar/css/simplebar.css') ?>" rel="stylesheet" />
+	<link href="<?= base_url('/plugins/perfect-scrollbar/css/perfect-scrollbar.css') ?>" rel="stylesheet" />
+	<link href="<?= base_url('/plugins/metismenu/css/metisMenu.min.css') ?>" rel="stylesheet" />
+	<link href="<?= base_url('/plugins/datatable/css/dataTables.bootstrap5.min.css') ?>" rel="stylesheet" />
+	<link href="<?= base_url('/plugins/highcharts/css/highcharts.css') ?>" rel="stylesheet" />
 	<!-- loader-->
-	<link href="<?= base_url('css/pace.min.css') ?>" rel="stylesheet" />
-	<script src="<?= base_url('js/pace.min.js') ?>"></script>
+	<link href="<?= base_url('/css/pace.min.css') ?>" rel="stylesheet" />
+	<script src="<?= base_url('/js/pace.min.js') ?>"></script>
 	<!-- Bootstrap CSS -->
-	<link href="<?= base_url('css/bootstrap.min.css') ?>" rel="stylesheet">
-	<link href="<?= base_url('css/bootstrap-extended.css') ?>" rel="stylesheet">
+	<link href="<?= base_url('/css/bootstrap.min.css') ?>" rel="stylesheet">
+	<link href="<?= base_url('/css/bootstrap-extended.css') ?>" rel="stylesheet">
 	<link href="https://fonts.googleapis.com/css2?family=Roboto:wght@400;500&display=swap" rel="stylesheet">
-	<link href="<?= base_url('css/app.css') ?>" rel="stylesheet">
-	<link href="<?= base_url('css/icons.css') ?>" rel="stylesheet">
+	<link href="<?= base_url('/css/app.css') ?>" rel="stylesheet">
+	<link href="<?= base_url('/css/icons.css') ?>" rel="stylesheet">
 	<!-- Theme Style CSS -->
-	<link rel="stylesheet" href="<?= base_url('css/dark-theme.css') ?>" />
-	<link rel="stylesheet" href="<?= base_url('css/semi-dark.css') ?>" />
-	<link rel="stylesheet" href="<?= base_url('css/header-colors.css') ?>" />
+	<link rel="stylesheet" href="<?= base_url('/css/dark-theme.css') ?>" />
+	<link rel="stylesheet" href="<?= base_url('/css/semi-dark.css') ?>" />
+	<link rel="stylesheet" href="<?= base_url('/css/header-colors.css') ?>" />
 	<!-- <link rel="stylesheet" href="<?= base_url('leaflet_map/leaflet.css') ?>" /> -->
 	<!-- <script src="<?= base_url('leaflet_map/leaflet.js') ?>"></script> -->
 
