e4c1b9157a5255dc67d7e4c201225344bfe0d05c
Mohammad Ainul <127191017+ainulfi@users.noreply.github.com>
2026-04-28 12:11:48 +0700

    Comment out the baseURL for production

--- app/Config/App.php
+++ app/Config/App.php
@@ -18,7 +18,7 @@ class App extends BaseConfig
      */
     // public string $baseURL = 'http://localhost:8080/';
     //public string $baseURL = DYNAMIC_BASE_URL;
-    public string $baseURL = 'https://pakkio-app.vercel.app/';
+    //public string $baseURL = 'https://pakkio-app.vercel.app/';
 
     /**
      * Allowed Hostnames in the Site URL other than the hostname in the baseURL.
